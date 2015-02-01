2b9fc4ce6826481a3a2f89a038d08ab8772abdde
Subrahmanyam <svvparakala@hotmail.com>
2015-01-31 19:58:33 -0800

    test

--- yes-no-maybe/ggs/yesnomaybe.js
+++ yes-no-maybe/ggs/yesnomaybe.js
@@ -1320,122 +1320,122 @@ function shuffle(array) {
 
 
 
-/** Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License
- */
+// /** Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  * use this file except in compliance with the License. You may obtain a copy of
+//  * the License at
+//  *
+//  * http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  * License for the specific language governing permissions and limitations under
+//  * the License
+//  */
 
-// This is alpha code for using OAuth2 in a Hangout.
-// It is likely that we will replace some or all of this functionality, and it
-// depends on the JavaScript API Client, which is currently in alpha as well.
+// // This is alpha code for using OAuth2 in a Hangout.
+// // It is likely that we will replace some or all of this functionality, and it
+// // depends on the JavaScript API Client, which is currently in alpha as well.
 
-// API key from the Google API Console
-// You can leave this as null if you are running inside the gadget.
-var apiKey = AIzaSyBxoGwcVTJUyX_oMkky4wF44ZD7ztE8Les;
+// // API key from the Google API Console
+// // You can leave this as null if you are running inside the gadget.
+// var apiKey = 'AIzaSyBxoGwcVTJUyX_oMkky4wF44ZD7ztE8Les';
 
-// Lay out the hangout scopes here.
-var scopes = ['https://www.googleapis.com/auth/plus.me',
-              'https://www.googleapis.com/auth/hangout.av',
-              'https://www.googleapis.com/auth/hangout.participants'];
+// // Lay out the hangout scopes here.
+// var scopes = ['https://www.googleapis.com/auth/plus.me',
+//               'https://www.googleapis.com/auth/hangout.av',
+//               'https://www.googleapis.com/auth/hangout.participants'];
 
-/** Run authorization call in either with or without popup
- * @param {boolean} isImmediate Use immedate mode for authorization.
- */
-function checkAuth(isImmediate) {
-  console.log('checking auth');
+// /** Run authorization call in either with or without popup
+//  * @param {boolean} isImmediate Use immedate mode for authorization.
+//  */
+// function checkAuth(isImmediate) {
+//   console.log('checking auth');
 
-  // Pass in a null client id; the gadget container will replace this
-  // with a generated one.
-  // HOWEVER!  You must go through the process of creating a client id
-  // associated with this hangout app in the Google API console.
-  gapi.auth.authorize({client_id: '172653159065-gggtic4pti02r8fja0sa1tgv8e012fe1.apps.googleusercontent.com',
-          scope: scopes,
-          immediate: isImmediate},
-    handleAuthResult);
-}
+//   // Pass in a null client id; the gadget container will replace this
+//   // with a generated one.
+//   // HOWEVER!  You must go through the process of creating a client id
+//   // associated with this hangout app in the Google API console.
+//   gapi.auth.authorize({client_id: '172653159065-gggtic4pti02r8fja0sa1tgv8e012fe1.apps.googleusercontent.com',
+//           scope: scopes,
+//           immediate: isImmediate},
+//     handleAuthResult);
+// }
 
-/** Callback from auth function
- * @param {object} authResult Return value from JS Client.
- */
+// /** Callback from auth function
+//  * @param {object} authResult Return value from JS Client.
+//  */
 
-function handleAuthResult(authResult) {
-  var authorizeButton = document.getElementById('authorize-button');
-  var tokenButton = document.getElementById('token-button');
+// function handleAuthResult(authResult) {
+//   var authorizeButton = document.getElementById('authorize-button');
+//   var tokenButton = document.getElementById('token-button');
 
-  if (authResult) {
-    authorizeButton.style.visibility = 'hidden';
-    tokenButton.style.visibility = '';
-    tokenButton.onclick = toggleTokenVisible;
-    makeGPlusApiCall();
-  } else {
-    authorizeButton.style.visibility = '';
-    tokenButton.style.visibility = 'hidden';
-    authorizeButton.onclick = onAuthorizeClick;
-  }
-}
+//   if (authResult) {
+//     authorizeButton.style.visibility = 'hidden';
+//     tokenButton.style.visibility = '';
+//     tokenButton.onclick = toggleTokenVisible;
+//     makeGPlusApiCall();
+//   } else {
+//     authorizeButton.style.visibility = '';
+//     tokenButton.style.visibility = 'hidden';
+//     authorizeButton.onclick = onAuthorizeClick;
+//   }
+// }
 
-function onAuthorizeClick(event) {
-  // non-immediate mode; i.e., pop up a dialog.
-  checkAuth(false);
-}
+// function onAuthorizeClick(event) {
+//   // non-immediate mode; i.e., pop up a dialog.
+//   checkAuth(false);
+// }
 
-function toggleTokenVisible(event) {
-  var tokenButton = document.getElementById('token-button');
-  var tokenField = document.getElementById('token-field');
+// function toggleTokenVisible(event) {
+//   var tokenButton = document.getElementById('token-button');
+//   var tokenField = document.getElementById('token-field');
 
-  if (tokenField.style.visibility == 'hidden') {
-    tokenField.innerHTML = gapi.auth.getToken().access_token;
-    tokenButton.innerHTML = 'Hide token';
-    tokenField.style.visibility = '';
-  } else {
-    tokenButton.innerHTML = 'Show token';
-    tokenField.style.visibility = 'hidden';
-  }
-}
+//   if (tokenField.style.visibility == 'hidden') {
+//     tokenField.innerHTML = gapi.auth.getToken().access_token;
+//     tokenButton.innerHTML = 'Hide token';
+//     tokenField.style.visibility = '';
+//   } else {
+//     tokenButton.innerHTML = 'Show token';
+//     tokenField.style.visibility = 'hidden';
+//   }
+// }
 
-function makeGPlusApiCall() {
-  gapi.client.load('plus', 'v1', function() {
-      var request = gapi.client.plus.people.get({
-          'userId': 'me'
-        });
-      request.execute(function(resp) {
-          var heading = document.createElement('h4');
-          var nameHeading = document.createElement('h2');
-          var image = document.createElement('img');
+// function makeGPlusApiCall() {
+//   gapi.client.load('plus', 'v1', function() {
+//       var request = gapi.client.plus.people.get({
+//           'userId': 'me'
+//         });
+//       request.execute(function(resp) {
+//           var heading = document.createElement('h4');
+//           var nameHeading = document.createElement('h2');
+//           var image = document.createElement('img');
 
-          // Note that these values, URL and displayName
-          // are already visible in the Hangouts API 
-          // as part of the Participant data structure.
-          // We are using these ONLY as an example.
-          image.src = resp.image.url;
-          nameHeading.appendChild(document.createTextNode(resp.displayName));
-          heading.appendChild(image);
+//           // Note that these values, URL and displayName
+//           // are already visible in the Hangouts API 
+//           // as part of the Participant data structure.
+//           // We are using these ONLY as an example.
+//           image.src = resp.image.url;
+//           nameHeading.appendChild(document.createTextNode(resp.displayName));
+//           heading.appendChild(image);
 
-          document.getElementById('content').appendChild(heading);
-          document.getElementById('content').appendChild(nameHeading);
+//           document.getElementById('content').appendChild(heading);
+//           document.getElementById('content').appendChild(nameHeading);
 
-          document.getElementById('instructions').innerHTML = '';
+//           document.getElementById('instructions').innerHTML = '';
 
-        });
-    });
-}
+//         });
+//     });
+// }
 
-/** Called when jsclient has fully loaded; sets API key */
-function onClientReady() {
-  gapi.hangout.onApiReady.add(function(event) {
-      console.log(event);
-      if (event.isApiReady) {
+// /** Called when jsclient has fully loaded; sets API key */
+// function onClientReady() {
+//   gapi.hangout.onApiReady.add(function(event) {
+//       console.log(event);
+//       if (event.isApiReady) {
 
-        gapi.client.setApiKey(apiKey);
-        window.setTimeout(function() { checkAuth(true) }, 1);
-      }
-    });
-}
+//         gapi.client.setApiKey(apiKey);
+//         window.setTimeout(function() { checkAuth(true) }, 1);
+//       }
+//     });
+// }
